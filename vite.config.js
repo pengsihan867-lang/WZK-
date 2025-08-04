105dd2d68b41d0a4f90623af2db5cda5ce13a3d6
pengsihan867-lang <pengsihan867@gmail.com>
2025-08-04 23:10:26 +1000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wuzekai-exam-simulator/', // 为GitHub Pages部署配置base路径
+  base: '/WZK-/', // 改成你的仓库名
 })
