5119c8d06b71161c010938e546bded28f40977bb
pengsihan867-lang <pengsihan867@gmail.com>
2025-08-04 23:03:52 +1000

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/wuzekai-exam-simulator/', // 为GitHub Pages部署配置base路径
+})
